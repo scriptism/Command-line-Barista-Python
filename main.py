4aff6e8fd590e1c4f12b4bd08eb556fa2ef86fd3
Amin <aminullahpuya@gmail.com>
2025-09-13 19:34:52 +0200

    peices added

--- main.py
+++ main.py
@@ -3,23 +3,46 @@ name = input("What is your name?\n")
 
 menu = (
     "Our menu for today is:\n"
-    "1. Black Coffee\n"
-    "2. cappuccino\n"
-    "3. Espresso\n"
-    "4. Latte\n"
-    "5. Mocha\n"
-    "6. Tea\n"
-    "7. Hot Chocolate\n"
-    "8. Iced Coffee\n"
-    "9. Smoothie\n"
-    "10. Frappuccino"
+    "1. Black Coffee -$2.00\n"
+    "2. cappuccino -$3.50\n"
+    "3. Espresso -$2.50\n"
+    "4. Latte -$4.00\n"
+    "5. Mocha -$4.50\n"
+    "6. Tea -$1.50\n"
+    "7. Hot Chocolate -$3.00\n"
+    "8. Iced Coffee -$4.00\n"
+    "9. Smoothie -$4.00\n"
+    "10. Frappuccino -$4.50"
 )
 
 print(f"Hello {name}! Welcome to scriptism!\n{menu}\n")
 print("What would you like to order?")
 order = input()
 
-price = 3.50  # default price
+# price 
+if order.lower() == "black coffee":
+    price = 2.00
+elif order.lower() == "cappuccino":
+    price = 3.50
+elif order.lower() == "espresso":
+    price = 2.50
+elif order.lower() == "latte":
+    price = 4.00
+elif order.lower() == "mocha":
+    price = 4.50
+elif order.lower() == "tea":
+    price = 1.50
+elif order.lower() == "hot chocolate":
+    price = 3.00
+elif order.lower() == "iced coffee":
+    price = 3.00
+elif order.lower() == "smoothie":
+    price = 4.00
+elif order.lower() == "frappuccino":
+    price = 4.50
+else:
+    print("Sorry, we don't have that item on the menu.")
+    exit()
 
 quantity = input(f"Great choice! How many {order} would you like to order?\n")
 total = float(price) * int(quantity)
